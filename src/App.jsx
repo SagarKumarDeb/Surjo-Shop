acf93a64009b7c711612ab4bcbed3ff08107d816
Sagar Kumar Deb <debsagar000@gmail.com>
2023-12-03 01:16:43 +0600

    added the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header/Header'
+import shop from './components/shop/shop'
+import { App } from './App.1'
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className='App'>
-      <Header></Header>
-    </div>
-  )
-}
 
 export default App;
