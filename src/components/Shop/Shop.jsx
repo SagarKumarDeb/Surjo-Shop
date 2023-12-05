559a0d3b34aa675ede74086c0f49e4a1d8380f97
Sagar Kumar Deb <debsagar000@gmail.com>
2023-12-05 22:31:50 +0600

    +Changes

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+import Product from './../Product/Product';
 
 const Shop = () => {
-    return (
-        <div>
-            <h2>Mira Rani Deb</h2>
+    const [products, setProducts] = useState([]);
+
+
+    useEffect( (() => { 
+        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
+     .then(res=>res.json()
+     ).then(data=>setProducts(data))}),[])
+    
+     return (
+        <div className='Shop-container'>
+            <div className='Products-container'>Products are coming here: {products.map (product=><Product 
+            key={product.id}
+            product={product}
+            
+            ></Product>)} </div>
+            <div className='Cart-container'>Order summary</div>
             
         </div>
     );
-};
+    };
 
 export default Shop;
